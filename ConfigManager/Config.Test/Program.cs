e5d60d3838d52e5fb3a45b48052a2b811e00cbfb
newcats <newcats@live.com>
2018-09-08 11:46:22 +0800

    添加Microsoft.Config源码

---
+++ ConfigManager/Config.Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Config.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            IConfiguration Configuration = builder.Build();
+            var sss = Configuration.GetConnectionString("des");
+            Console.WriteLine($"option1 = {Configuration["Option1"]}");
+            Console.WriteLine($"option2 = {Configuration["option2"]}");
+            Console.WriteLine($"suboption1 = {Configuration["subsection:suboption1"]}");
+            Console.WriteLine();
+            Console.WriteLine("Wizards:");
+            Console.Write($"{Configuration["wizards:0:Name"]}, ");
+            Console.WriteLine($"age {Configuration["wizards:0:Age"]}");
+            Console.Write($"{Configuration["wizards:1:Name"]}, ");
+            Console.WriteLine($"age {Configuration["wizards:1:Age"]}");
+            Console.WriteLine();
+            Console.WriteLine("Press a key...");
+            Console.ReadKey();
+        }
+    }
+}
